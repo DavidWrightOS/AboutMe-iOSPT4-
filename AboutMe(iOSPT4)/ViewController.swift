8df230cb6b15bc81b39147f6b5643d844f9a2c9e
David Wright <davidtwright@gmail.com>
2019-10-31 21:57:56 -0400

    Initial Commit

---
+++ AboutMe(iOSPT4)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe(iOSPT4)
+//
+//  Created by David Wright on 10/31/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
